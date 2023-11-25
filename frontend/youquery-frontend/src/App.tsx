a0d5bc6100225e82aadc31c33ae45d62988d0987
zakt1 <you@example.com>
2023-11-25 20:34:50 +0000

    feat: VideoSummary page skeleton & mui theme setup

--- frontend/youquery-frontend/src/App.tsx
+++ frontend/youquery-frontend/src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { VideoSummaryPage } from './Pages/VideoSummaryPage';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './styles';
+import { Container } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xl" sx={{backgroundColor: 'primary.main'}}>
+
+<VideoSummaryPage/>
+      </Container>
+
+    </ThemeProvider>
   );
 }
 
